2165e40505a8e6705255284b07693b56fdf734bc
gacmen45 <marekgacek45@gmail.com>
2023-01-21 10:57:02 +0100

    finish footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 const hamburgerBtn  = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav__list-mobile')
+const footerYearSpan = document.querySelector('.footer__bottom-text--year')
 
 const hamburgerHandler = () => {
 hamburgerBtn.classList.toggle('is-active')
 navList.classList.toggle('nav__list-mobile--active')
+}
 
+const footerYear = () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    
+    footerYearSpan.innerHTML = year
 }
 
 
-
-
 hamburgerBtn.addEventListener('click',hamburgerHandler)
+
+footerYear()
