7a08dc3f26878c61311784307587a874acca2376
gacmen45 <marekgacek45@gmail.com>
2023-01-21 17:28:27 +0100

    add gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,38 @@
-const hamburgerBtn  = document.querySelector('.hamburger')
+const hamburgerBtn = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav__list-mobile')
 const footerYearSpan = document.querySelector('.footer__bottom-text--year')
 
 const hamburgerHandler = () => {
-hamburgerBtn.classList.toggle('is-active')
-navList.classList.toggle('nav__list-mobile--active')
+	hamburgerBtn.classList.toggle('is-active')
+	navList.classList.toggle('nav__list-mobile--active')
 }
 
 const footerYear = () => {
-    const date = new Date()
-    const year = date.getFullYear()
-    
-    footerYearSpan.innerHTML = year
+	const date = new Date()
+	const year = date.getFullYear()
+
+	footerYearSpan.innerHTML = year
 }
 
+const buttons = document.querySelectorAll('[data-carousel-button]')
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]")
+  
+      const activeSlide = slides.querySelector("[data-active]")
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0) newIndex = slides.children.length - 1
+      if (newIndex >= slides.children.length) newIndex = 0
+  
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
+	})
+})
 
-
-
-
-hamburgerBtn.addEventListener('click',hamburgerHandler)
+hamburgerBtn.addEventListener('click', hamburgerHandler)
 
 footerYear()
