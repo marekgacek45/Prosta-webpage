d8f97d61bce2032fa570e4b72156ce4b68ee12e6
gacmen45 <marekgacek45@gmail.com>
2023-01-22 09:03:31 +0100

    add handle observer and auto-play to gallery

--- src/js/main.js
+++ src/js/main.js
@@ -14,24 +14,50 @@ const footerYear = () => {
 	footerYearSpan.innerHTML = year
 }
 
+//KARUZELA
 const buttons = document.querySelectorAll('[data-carousel-button]')
 
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      const offset = button.dataset.carouselButton === "next" ? 1 : -1
-      const slides = button
-        .closest("[data-carousel]")
-        .querySelector("[data-slides]")
-  
-      const activeSlide = slides.querySelector("[data-active]")
-      let newIndex = [...slides.children].indexOf(activeSlide) + offset
-      if (newIndex < 0) newIndex = slides.children.length - 1
-      if (newIndex >= slides.children.length) newIndex = 0
-  
-      slides.children[newIndex].dataset.active = true
-      delete activeSlide.dataset.active
+	button.addEventListener('click', () => {
+		const offset = button.dataset.carouselButton === 'next' ? 1 : -1
+		const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
+
+		const activeSlide = slides.querySelector('[data-active]')
+		let newIndex = [...slides.children].indexOf(activeSlide) + offset
+		if (newIndex < 0) newIndex = slides.children.length - 1
+		if (newIndex >= slides.children.length) newIndex = 0
+
+		slides.children[newIndex].dataset.active = true
+		delete activeSlide.dataset.active
 	})
 })
+
+const carousel = document.querySelector('[data-carousel]')
+const interval = 5000 //
+const nextButton = carousel.querySelector("[data-carousel-button='next']")
+
+setInterval(() => {
+	nextButton.click()
+}, interval)
+
+//handle observer
+const navLinks = document.querySelectorAll('.nav__list-item')
+const sections = document.querySelectorAll('section')
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset + (window.innerHeight/3);
+  for (let i = 0; i < sections.length; i++) {
+    if (currentScrollPos >= sections[i].offsetTop) {
+      const id = sections[i].getAttribute('id');
+      for (let j = 0; j < navLinks.length; j++) {
+        navLinks[j].classList.remove('active');
+        if (navLinks[j].getAttribute('href') == '#' + id) {
+          navLinks[j].classList.add('active');
+        }
+      }
+    }
+  }
+}
 
 hamburgerBtn.addEventListener('click', hamburgerHandler)
 
